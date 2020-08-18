e2e02e56878e0c031eb4d2f9b4db6d422342c236
gatchrat <henkolt@t-online.de>
2020-08-18 17:20:50 +0200

    Added Setting gui

--- Example/Gui.java
+++ Example/Gui.java
@@ -2,6 +2,7 @@
 import gifH.GifH;
 import javafx.application.Application;
 import javafx.scene.*;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -11,50 +12,190 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
+
 public class Gui extends Application {
+    BorderPane root;
+    Scene scene;
+    File file = null;
+    ArrayList<File> files;
     public static void launch() {
         Application.launch(Gui.class);
     }
     public void start(Stage stage) throws Exception {
-        BorderPane root = new BorderPane();
-        HBox mainMenu = new HBox();
-        Scene scene = new Scene(root, 640, 480);
-        Button generate = new Button ("Generate Gif");
-        FileChooser fileChooser = new FileChooser();
+        root = new BorderPane();
+        scene = new Scene(root, 640, 480);
+        stage.setTitle("GifH");
+        stage.setScene(scene);
+        stage.show();
+        createMainMenu(stage);
 
-        generate.setOnAction(new EventHandler<ActionEvent>() {
- 
+
+    }
+    private void createGenerateSettingsMenu(Stage stage) {
+
+        resetGUI();
+        HBox settingMenu = new HBox();
+        HBox startMenu = new HBox();
+        HBox backMenu = new HBox();
+        Button generate = new Button ("Generate Gif");
+        Button back = new Button("Back");
+        FileChooser fileChooser = new FileChooser();
+        back.setOnAction(new EventHandler<ActionEvent>() {
+
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("ye");
+                createMainMenu(stage);
+            }
+        });
+        generate.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(file != null){
+                    try {
+                        GifH gifH = new GifH();
+                    } catch (Exception e) {
+                        System.out.println("Couldn't read the file");
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("No File selected");
+
+                    alert.showAndWait();
+                }
+
+
+            }
+        });
+        Button chooseFile = new Button ("Choose Files");
+        chooseFile.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                fileChooser.setTitle("Open files");
+
+                files = (ArrayList<File>) fileChooser.showOpenMultipleDialog(stage);
+                System.out.println("Reading " + file.getName());
+
+
+            }
+        });
+        startMenu.setAlignment(Pos.TOP_CENTER);
+        startMenu.getChildren().add(generate);
+
+        backMenu.setAlignment(Pos.BOTTOM_CENTER);
+        backMenu.getChildren().add(back);
+
+        settingMenu.setAlignment(Pos.CENTER);
+        settingMenu.getChildren().add(chooseFile);
+        root.setCenter(settingMenu);
+        root.setBottom(startMenu);
+        root.setLeft(backMenu);
+    }
+    private void createExtractSettingsMenu(Stage stage) {
+
+        resetGUI();
+        HBox settingMenu = new HBox();
+        HBox startMenu = new HBox();
+        HBox backMenu = new HBox();
+        Button generate = new Button ("Extract Gif");
+        Button back = new Button("Back");
+        FileChooser fileChooser = new FileChooser();
+        back.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                createMainMenu(stage);
+            }
+        });
+        generate.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                if(file != null){
+                    try {
+                        byte[] fileContent = Files.readAllBytes(file.toPath());
+                        GifH gifH = new GifH();
+                        gifH.extractFilesFromGif(fileContent);
+                    } catch (Exception e) {
+                        System.out.println("Couldn't read the file");
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("No File selected");
+
+                    alert.showAndWait();
+                }
+
+
+            }
+        });
+        Button chooseFile = new Button ("Choose Files");
+        chooseFile.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                fileChooser.setTitle("Open file to extract");
+
+                file = fileChooser.showOpenDialog(stage);
+                System.out.println("Reading " + file.getName());
+
+
+            }
+        });
+        startMenu.setAlignment(Pos.TOP_CENTER);
+        startMenu.getChildren().add(generate);
+
+        backMenu.setAlignment(Pos.BOTTOM_CENTER);
+        backMenu.getChildren().add(back);
+
+        settingMenu.setAlignment(Pos.CENTER);
+        settingMenu.getChildren().add(chooseFile);
+        root.setCenter(settingMenu);
+        root.setBottom(startMenu);
+        root.setLeft(backMenu);
+    }
+    private void resetGUI(){
+        root.setLeft(null);
+        root.setRight(null);
+        root.setBottom(null);
+        root.setCenter(null);
+        root.setTop(null);
+    }
+    private void createMainMenu(Stage stage){
+        resetGUI();
+        HBox mainMenu = new HBox();
+
+        Button generate = new Button ("Generate Gif");
+        generate.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+               createGenerateSettingsMenu(stage);
             }
         });
         Button extract = new Button ("Extract images from Gif");
         extract.setOnAction(new EventHandler<ActionEvent>() {
- 
+
             @Override
             public void handle(ActionEvent event) {
-                fileChooser.setTitle("Open file to extract");
-                File file = fileChooser.showOpenDialog(stage);
-                System.out.println("Reading " + file.getName());
-                try {
-                    byte[] fileContent = Files.readAllBytes(file.toPath());
-                    GifH gifH = new GifH();
-                    gifH.extractFilesFromGif(fileContent);
-                } catch (Exception e) {
-                   System.out.println("Couldnt read the file");
-                   e.printStackTrace();
-                }
-               
+                createExtractSettingsMenu(stage);
+
             }
         });
         mainMenu.setAlignment(Pos.CENTER);
         mainMenu.getChildren().add(generate);
         mainMenu.getChildren().add(extract);
         root.setCenter(mainMenu);
-        stage.setTitle("Example.GifH");
-        stage.setScene(scene);
-        stage.show();
 
     }
     
--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # gifH
-State:
+
+## Usage:
+1. Import the gifH.jar into your Project
+2. Look into the Example and Documentation
+3. Use
+
+## State:
 Can extract all Images from a gif and saves them in a folder.
 
 
-Missing:
-Packing
-Transparency
-Background
-Disposal Methods (2-7) (Currently all implemented as method 1 )
+## Missing:
+* Packing
+* Transparency
+* Background
+* Disposal Methods (2-7) (Currently all implemented as method 1 )
